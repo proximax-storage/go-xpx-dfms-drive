9166efe9df375792fe918abbefd5b14b27c59a5f
Evgeny Ukhanov <mrlsd@ya.ru>
2019-10-24 23:30:35 +0300

    Change Invite structure

--- invite.go
+++ invite.go
@@ -1,6 +1,7 @@
 package drive
 
 import (
+	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/peer"
 )
 
@@ -8,9 +9,10 @@ var NilInvite = Invite{}
 
 // Invite represents invitation for replicators to join Contract as a member.
 type Invite struct {
-	Drive    ID
-	Created  uint64
-	Duration uint64
-	Space    uint64
-	Owner    peer.ID
+	Drive      ID
+	Created    uint64
+	Duration   uint64
+	Space      uint64
+	Owner      peer.ID
+	ContractID crypto.PubKey
 }
