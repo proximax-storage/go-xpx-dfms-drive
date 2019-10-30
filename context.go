dcf0e288b56ce775bf82f831a94c9a4110f93027
vadimdidenkogs <vadim.didenko@482.solutions>
2019-10-30 16:11:57 +0200

    Change context returned type

--- context.go
+++ context.go
@@ -6,20 +6,20 @@ import (
 
 const contractKey = "drive-contract"
 
-func ContextWithContract(ctx context.Context, contract Contract) context.Context {
+func ContextWithContract(ctx context.Context, contract *BasicContract) context.Context {
 	return context.WithValue(ctx, contractKey, contract)
 }
 
-func ContractFromContext(ctx context.Context) Contract {
+func ContractFromContext(ctx context.Context) *BasicContract {
 	value := ctx.Value(contractKey)
 	if value == nil {
 		return nil
 	}
 
-	d, ok := value.(Contract)
+	d, ok := value.(BasicContract)
 	if !ok {
 		panic("context value for contractKey is not a Drive")
 	}
 
-	return d
+	return &d
 }
